6a09df0363cb388e642f675da841006aa366cd2f
HanJin Park <phj9020@hotmail.com>
2021-07-11 18:18:32 +0900

    Delete build directory

--- build/main.js
+++
@@ -1,9 +0,0 @@
-import { PageComponent } from "./component/page.js";
-class App {
-    constructor(appRoot) {
-        this.page = new PageComponent();
-        this.page.attachTo(appRoot);
-    }
-}
-;
-new App(document.querySelector(".content_board_container"));
